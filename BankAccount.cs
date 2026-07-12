d65b195548b2dc760829532204abfa422fbba677
Abhinash Panigrahi <abhinashpanigrahi9@gmail.com>
2026-07-12 23:09:33 +0530

    Changes in the Files

---
+++ BankAccount.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class BankAccount
+{
+    // Private Fields
+    private string accountHolder;
+    private double balance;
+
+    // Property for Account Holder
+    public string AccountHolder
+    {
+        get
+        {
+            return accountHolder;
+        }
+
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                Console.WriteLine("Account Holder Name cannot be empty.");
+            }
+            else
+            {
+                accountHolder = value;
+            }
+        }
+    }
+
+    // Property for Balance
+    public double Balance
+    {
+        get
+        {
+            return balance;
+        }
+
+        set
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("Balance cannot be negative.");
+            }
+            else
+            {
+                balance = value;
+            }
+        }
+    }
+
+    // Method
+    public void ShowDetails()
+    {
+        Console.WriteLine("\n===== ACCOUNT DETAILS =====");
+        Console.WriteLine("Account Holder : " + accountHolder);
+        Console.WriteLine("Balance        : " + balance);
+    }
+}
